d4670c71f5e2defdc1b61a4fc5d9e225b7af2756
Matthew Knudsen <mknudsen01@gmail.com>
2014-03-08 19:53:00 -0800

    Create a board that converts a puzzle string into cells
    * Create board that takes a string of cell values
    * Create board with empty cells array
    * Create "createBoard", which splits the string, makes
      a cell out of each value with the value and position in
      the string, and gets the row, column and box of each cell

---
+++ spec/boardSpec.js
@@ -0,0 +1,19 @@
+describe("SudokuApp.Board", function(){
+  var puzzleString = '096040001100060004504810390007950043030080000405023018010630059059070830003590007';
+
+  it("should create a new board", function(){
+    expect(new SudokuApp.Board(puzzleString)).toBeDefined();
+  });
+
+  it("should be created with an empty cells array", function(){
+    expect(new SudokuApp.Board(puzzleString).cells.length).toEqual(0);
+  });
+
+  describe("createBoard", function(){
+    it("should add 81 cells to the board cells", function(){
+      board = new SudokuApp.Board(puzzleString);
+      board.createBoard();
+      expect(board.cells.length).toEqual(81);
+    });
+  });
+});
