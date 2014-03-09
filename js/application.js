4eff866273751c1706ff35532fd68153b2c4eb14
Matthew Knudsen <mknudsen01@gmail.com>
2014-03-08 18:02:32 -0800

    Create cells that can obtain their row, column and box
    * Created an html skeleton to load in the javascript file
    * Created a Cell object that takes a value and an index position.
    * From this position, the cell can determine:
      * its row,
      * its column,
      * its box (1 - 9, where boxes are counted left to right, top to
        bottom)
    * The cell will need to run setRowColBox() in order to assign all
      of the values.
    
    Next steps:
    * Testing! I shouldn't have done all of this without tests, so the
      next step is to get jasmine configured and testing that cells
      are made and correctly calculate their rows, columns, and boxes.

---
+++ js/application.js
@@ -0,0 +1,38 @@
+var SudokuApp = {};
+
+////START CELL
+SudokuApp.Cell = function(value, position){
+  this.value = value;
+  this.position = position;
+  this.row = 0;
+  this.column = 0;
+  this.box = 0;
+};
+
+SudokuApp.Cell.prototype = {
+  setRowColBox: function(){
+    this.getRow(this.position);
+    this.getColumn(this.position);
+    this.getBox(this.row, this.column);
+  },
+
+  getRow: function(position){
+    this.row = 1 + Math.floor(position / 9);
+  },
+
+  getColumn: function(position){
+    this.column = 1 + (position % 9);
+  },
+
+  getBox: function(row, column){
+    boxRow = Math.ceil(this.row / 3);
+    boxCol = Math.ceil(this.column / 3);
+    this.assignBox(boxRow, boxCol);
+  },
+
+  assignBox: function(row, column){
+    this.box = (row-1) * 3 + column;
+  }
+};
+////END CELL
+
